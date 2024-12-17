60ca99710728fc923420b8e7c64b8b8dfa60e1a2
j-t-1 <120829237+j-t-1@users.noreply.github.com>
2024-12-17 20:40:54 +0000

    Use generator expressions
    PEP 289 – Generator Expressions.

--- volatility3/framework/constants/_version.py
+++ volatility3/framework/constants/_version.py
@@ -5,7 +5,7 @@ VERSION_PATCH = 0  # Number of changes that do not change the interface
 VERSION_SUFFIX = ""
 
 PACKAGE_VERSION = (
-    ".".join([str(x) for x in [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH]])
+    ".".join(str(x) for x in [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH])
     + VERSION_SUFFIX
 )
 """The canonical version of the volatility3 package"""
