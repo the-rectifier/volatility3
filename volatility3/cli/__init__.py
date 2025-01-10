1cf0232d25fa6dffa21ae3c281e1f568bbb280ab
Abyss Watcher <abyss-w4tcher@proton.me>
2025-01-10 21:19:31 +0100

    less specific argparse epilog reference

--- volatility3/cli/__init__.py
+++ volatility3/cli/__init__.py
@@ -367,7 +367,7 @@ class CommandLine:
                 plugin,
                 help=plugin_list[plugin].__doc__,
                 description=plugin_list[plugin].__doc__,
-                epilog=getattr(plugin_list[plugin], "_argparse_epilog", None),
+                epilog=plugin_list[plugin].additional_description,
             )
             self.populate_requirements_argparse(plugin_parser, plugin_list[plugin])
 
